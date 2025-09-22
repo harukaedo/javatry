6154fe257087c5ed7f676027775aa9e99a7d27ca
harukaedo <h.edo@bizreach.co.jp>
2025-09-22 18:08:41 +0900

    0922：refoctor/ TicketBoothのDocのリファクタリング

--- src/main/java/org/docksidestage/bizfw/basic/buyticket/TicketBooth.java
+++ src/main/java/org/docksidestage/bizfw/basic/buyticket/TicketBooth.java
@@ -61,6 +61,27 @@ public class TicketBooth {
      * @throws TicketSoldOutException When ticket in booth is sold out.
      * @throws TicketShortMoneyException When the specified money is short for purchase.
      */
+
+    // ============================================================================================
+    //                                                                          Buy Two-day Ticket
+    //                                                                          ===================
+    // you can rewrite comments for your own language by jflute
+    // e.g. Japanese
+    // /**
+    // * 2Dayパスポートを買う,お釣りを返すメソッド。パークゲスト用のメソッド。
+    // * @param handedMoney パークゲストから手渡しされたお金(金額) (NotNull, NotMinus)
+    // * @throws TicketSoldOutException ブース内のチケットが売り切れだったら
+    // * @throws TicketShortMoneyException 買うのに金額が足りなかったら
+    // * @return お釣り金額
+    // */
+    // TODO　done edo 戻り値があるので、JavaDocの@returnをぜひ追加してみてください by jflute (2025/09/19)
+    /**
+     * Buy two-day passport, method for park guest.
+     * @param handedMoney The money (amount) handed over from park guest. (NotNull, NotMinus)
+     * @throws TicketSoldOutException When ticket in booth is sold out.
+     * @throws TicketShortMoneyException When the specified money is short for purchase.
+     */
+
     public void buyOneDayPassport(Integer handedMoney) {
         if (quantity <= 0) {
             throw new TicketSoldOutException("Sold out");
@@ -94,42 +115,6 @@ public class TicketBooth {
         return change;
     }
 
-    public static class TicketSoldOutException extends RuntimeException {
-
-        private static final long serialVersionUID = 1L;
-
-        public TicketSoldOutException(String msg) {
-            super(msg);
-        }
-    }
-
-    public static class TicketShortMoneyException extends RuntimeException {
-
-        private static final long serialVersionUID = 1L;
-
-        public TicketShortMoneyException(String msg) {
-            super(msg);
-        }
-    }
-
-    // ============================================================================================
-    //                                                                          Buy Two-day Ticket
-    //                                                                          ===================
-    // you can rewrite comments for your own language by jflute
-    // e.g. Japanese
-    // /**
-    // * 2Dayパスポートを買う,お釣りを返すメソッド。パークゲスト用のメソッド。
-    // * @param handedMoney パークゲストから手渡しされたお金(金額) (NotNull, NotMinus)
-    // * @throws TicketSoldOutException ブース内のチケットが売り切れだったら
-    // * @throws TicketShortMoneyException 買うのに金額が足りなかったら
-    // */
-    // TODO edo 戻り値があるので、JavaDocの@returnをぜひ追加してみてください by jflute (2025/09/19)
-    /**
-     * Buy two-day passport, method for park guest.
-     * @param handedMoney The money (amount) handed over from park guest. (NotNull, NotMinus)
-     * @throws TicketSoldOutException When ticket in booth is sold out.
-     * @throws TicketShortMoneyException When the specified money is short for purchase.
-     */
     public int buyTwoDayPassport(Integer handedMoney) {
         if (quantity <= 2) {
             throw new TicketSoldOutException("Sold out");
@@ -149,7 +134,24 @@ public class TicketBooth {
     
     // ===================================================================================
     //                                                                           Exception
-    //                                                                           =========
+    //           
+    public static class TicketSoldOutException extends RuntimeException {
+
+        private static final long serialVersionUID = 1L;
+
+        public TicketSoldOutException(String msg) {
+            super(msg);
+        }
+    }
+
+    public static class TicketShortMoneyException extends RuntimeException {
+
+        private static final long serialVersionUID = 1L;
+
+        public TicketShortMoneyException(String msg) {
+            super(msg);
+        }
+    }                                                               
 
     // ===================================================================================
     //                                                                            Accessor
