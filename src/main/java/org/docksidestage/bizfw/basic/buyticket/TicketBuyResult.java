f19d22106c993bd6353840e743d16cd23b435d7a
harukaedo <h.edo@bizreach.co.jp>
2025-09-27 19:03:03 +0900

    0926：refactor:TicketResultにjavadocを追加

--- src/main/java/org/docksidestage/bizfw/basic/buyticket/TicketBuyResult.java
+++ src/main/java/org/docksidestage/bizfw/basic/buyticket/TicketBuyResult.java
@@ -1,19 +1,44 @@
 package org.docksidestage.bizfw.basic.buyticket;
 
+/**
+ * チケット購入結果を表すクラス。
+ * 購入されたチケットとお釣り金額の管理をする。
+ * 
+ * @author jflute
+ * @author harukaedo
+ */
+
 public class TicketBuyResult {
 
     private final Ticket ticket;
     private final int change;
 
+    /**
+     * チケット購入結果を構築します。
+     * 
+     * @param ticket 購入されたチケット (NotNull)
+     * @param change お釣り金額 (NotMinus)
+     */
+
     public TicketBuyResult(Ticket ticket, int change) {
-        this.ticket = ticket;
+        this.ticket = ticket;   
         this.change = change;
     }
 
+    /**
+     * 購入されたチケットを取得します。
+     * 
+     * @return 購入されたチケット
+     */
     public Ticket getTicket() {
         return ticket;
     }
 
+    /**
+     * お釣り金額を取得します。
+     * 
+     * @return お釣り金額 (NotMinus)
+     */
     public int getChange() {
         return change;
     }
